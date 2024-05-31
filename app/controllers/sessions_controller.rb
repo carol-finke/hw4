1ace7650312d6f2a6752529533f5e8d2400f416f
Carol Finke <carol.finke@kellogg.northwestern.edu>
2024-05-31 14:22:55 +0000

    may 31 updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
-
+  
   def create
+    @user = User.find_by(email: params[:email])
+    if @user && BCrypt::Password.new(@user.password) == params[:password]
+      session[:user_id] = @user.id
+      flash[:notice] = "Hello."
+      redirect_to places_path
+    else
+      flash[:notice] = "Invalid email or password."
+      redirect_to login_path
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Goodbye."
+    redirect_to login_path
   end
 end
-  
